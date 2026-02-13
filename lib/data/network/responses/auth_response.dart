77699d1018e78d360da3f88a1266499f6216fa28
siljavind <silja.vj@gmail.com>
2026-02-13 01:32:18 +0100

    Update auth_response.dart

--- lib/data/network/responses/auth_response.dart
+++ lib/data/network/responses/auth_response.dart
@@ -1,23 +1,12 @@
-/// Represents the authentication response from the backend.
-///
-/// This DTO is used to parse the JSON response from the login and register
-/// endpoints, providing the access token, its expiration date, and the
-/// refresh token.
+//TODO Remove refreshToken?
 class AuthResponse {
   /// The JWT access token used for authenticating subsequent API requests.
   final String accessToken;
-
-  /// The expiration date and time of the access token.
-  final DateTime expiresAt;
-
-  /// The refresh token used to obtain a new access token when the current one
-  /// expires.
   final String refreshToken;
 
   /// Creates a new instance of [AuthResponse].
   AuthResponse({
     required this.accessToken,
-    required this.expiresAt,
     required this.refreshToken,
   });
 
@@ -27,7 +16,6 @@ class AuthResponse {
   factory AuthResponse.fromJson(Map<String, dynamic> json) {
     return AuthResponse(
       accessToken: json['accessToken'],
-      expiresAt: DateTime.parse(json['expiresAt']),
       refreshToken: json['refreshToken'],
     );
   }
